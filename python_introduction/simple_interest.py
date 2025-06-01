6fa6925f922fed55a81006012f281586e58214c2
ODOYO CORNELLIUS NYASWA <107390845+odoyocorneliusnyaswa@users.noreply.github.com>
2025-06-01 10:23:03 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 principal = 1000  #Kshs 1000
 rate = 0.05       #5% annual interest rate
 time = 3          # 3 years
-interest = "principle * rate * time"
+interest = "principal * rate * time"
 print ("interest")
 print(f"The simple interest is: {interest}")
